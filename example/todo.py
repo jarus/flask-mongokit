4fa000fd6520f6cefd6bca11fa78fdadcadeaba7
Christoph Heer <Christoph.Heer@googlemail.com>
2011-08-16 21:19:08 +0200

    Add a simple todo application as example of the simple usage of MongoKit in Flask

---
+++ example/todo.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+
+from flask import Flask, request, render_template, redirect, url_for
+from flaskext.mongokit import MongoKit, Document
+
+app = Flask(__name__)
+
+class Task(Document):
+    __collection__ = 'tasks'
+    structure = {
+        'title': unicode,
+        'text': unicode,
+        'creation': datetime,
+    }
+    required_fields = ['title', 'creation']
+    default_values = {'creation': datetime.utcnow()}
+    use_dot_notation = True 
+    
+db = MongoKit(app)
+db.register([Task])
+
+@app.route('/')
+def show_all():
+    tasks = db.Task.find()
+    return render_template('list.html', tasks=tasks)
+
+@app.route('/<MongoDBObjectID:task_id>')
+def show_task(task_id):
+    task = db.Task.get_from_id(task_id)
+    return render_template('task.html', task=task)
+
+@app.route('/new', methods=["GET", "POST"])
+def new_task():
+    if request.method == 'POST':
+        task = db.Task()
+        task.title = request.form['title']
+        task.text = request.form['text']
+        task.save()
+        return redirect(url_for('show_all'))
+    return render_template('new.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

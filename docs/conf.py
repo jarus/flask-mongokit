c9a639a087d19b624ea86e6d330dda52191d4a23
Christoph Heer <Christoph.Heer@googlemail.com>
2011-08-17 20:26:44 +0200

    Add logo

--- docs/conf.py
+++ docs/conf.py
@@ -96,7 +96,10 @@ html_theme = 'flask_small'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'github_fork': None}
+html_theme_options = {
+    'github_fork': None,
+    'index_logo': 'flask-mongokit.png'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['_themes']

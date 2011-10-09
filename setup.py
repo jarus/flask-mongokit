fd1689641ff382e35a448ed7dfbbba2bc854117b
Christoph Heer <Christoph.Heer@gmail.com>
2011-10-09 10:33:03 +0200

    Set version number to 0.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='Flask-MongoKit',
-    version='0.2',
+    version='0.3',
     url='http://github.com/jarus/flask-mongokit',
     license='BSD',
     author='Christoph Heer',

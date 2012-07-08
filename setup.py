f7204cf539e2c06a9a37508d9ad4004219d149d2
Christoph Heer <Christoph.Heer@gmail.com>
2012-07-08 10:22:04 +0200

    Bump version to 0.6

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='Flask-MongoKit',
-    version='0.5',
+    version='0.6',
     url='http://github.com/jarus/flask-mongokit',
     license='BSD',
     author='Christoph Heer',
